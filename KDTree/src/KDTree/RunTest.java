3478dc2a2811610f87b78c22326f9aca309c7d07
xiexiangyi0 <xiangyixie2014@u.northwestern.edu>
2014-04-22 16:44:53 -0500

    new file:   RunTest.java 	new file:   io.java

---
+++ KDTree/src/KDTree/RunTest.java
@@ -0,0 +1,34 @@
+/*Run test for k-d tree index.*/
+import java.util.HashMap;
+import java.util.Random;
+
+public class RunTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final int testnum = 1500;
+		Btree<Long> b = new Btree<Long>();
+		Random r = new Random();
+	
+		HashMap<Integer,Long> hm = new HashMap<Integer,Long>();
+		Long random_value;
+		for(int i = 1; i< testnum; i++){
+			random_value = r.nextLong();
+			b.insert(i, random_value);
+			hm.put(i, random_value);
+		}
+		
+		/*b.DebugPrint();*/
+		/**/
+		for(int i=1; i<testnum; i++){
+			Long v1 = b.find(i) ;
+			Long v2 =  hm.get(i);
+			if(v1 == null || v1.compareTo(v2) != 0)
+				System.out.println("["+String.valueOf(i) +"] ERROR: "+v1.toString() +" <> "+ v2.toString());
+			else System.out.println("PASS. [KEY]:"+ String.valueOf(i) + " [VALUE]:"+Long.toHexString(v1));
+		}
+	}
+
+}
---
+++ KDTree/src/KDTree/io.java
@@ -0,0 +1,63 @@
+/*I/O on fs for run test k-d tree index.*/
+import java.io.*;
+
+interface ISerialize<T extends java.io.Serializable> {
+	public boolean write(T t, int block);
+	public T read(int block);
+}
+
+/*
+ * Java IO Serializer 
+ * On native file System
+ */
+class nativeSerializer<T extends java.io.Serializable> implements ISerialize<T> {
+	//private Class<? extends Serializable> obj;
+	private FileInputStream fileIn;
+	private ObjectInputStream in;
+		
+	private FileOutputStream fileOut;
+	private ObjectOutputStream out;
+	
+	public nativeSerializer(){
+		//obj = o.getClass();
+	}
+	public boolean write(T t, int block) {
+		try{
+			fileOut = new FileOutputStream(String.valueOf(block));
+			out = new ObjectOutputStream(fileOut);
+			out.writeObject(t);
+			out.close();
+			fileOut.close();
+			return true;
+        }catch(Exception ex){
+        	//System.out.println(ex.toString()+"!!!write error!!!");
+        	return false;
+        }
+	}
+
+	@SuppressWarnings("unchecked")
+	public T read(int block) {
+		try{
+			//T t = (T) obj.newInstance();
+			//System.out.println("Reading block: "+String.valueOf(block));
+			fileIn = new FileInputStream(String.valueOf(block));
+			in = new ObjectInputStream(fileIn);
+			T t = (T) in.readObject();
+			in.close();
+			fileIn.close();
+			return t;
+		} catch(Exception ex){
+			//System.out.println(ex.toString()+"!!!read error!!!");
+			return null;
+		}
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {  
+			fileIn.close();
+			fileOut.close();
+			in.close();
+			out.close();
+		    super.finalize();
+	}  
+}
